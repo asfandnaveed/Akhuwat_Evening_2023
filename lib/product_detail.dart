5f845552f84596a4a250ddfc77897ec1ec642009
asfand naveed <m.asfandnaveed@gmail.com>
2023-07-31 13:58:09 +0500

    changes

--- lib/product_detail.dart
+++ lib/product_detail.dart
@@ -24,6 +24,71 @@ class ProductDetail extends StatelessWidget {
               Container(
                 child: Image.asset('assets/images/burger.png'),
               ),
+
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Veg Burger',
+                    style: TextStyle(
+                      fontSize: 23,
+                      fontWeight: FontWeight.w600
+                    ),
+                  ),
+
+                  Container(
+                    child: Row(
+                      children: [
+                        Card(
+                          color: Color(0xffe2dcd7),
+                          elevation: 6.0,
+                          child: Container(
+                            width: 40,
+                            height: 40,
+                            child: Center(
+                              child: Text(
+                                '-',
+                                style: TextStyle(
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w600,
+                                  color: Color(0xffc6c0bb),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Text(
+                          '01',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Card(
+                          color: Color(0xffcd461d),
+                          elevation: 6.0,
+                          shadowColor: Color(0xffcd461d),
+                          child: Container(
+                            width: 40,
+                            height: 40,
+                            child: Center(
+                              child: Text(
+                                '+',
+                                style: TextStyle(
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
