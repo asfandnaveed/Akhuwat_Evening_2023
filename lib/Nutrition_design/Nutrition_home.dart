9016ba11f7dec113db8346b88d8a09fbe1eb3695
asfand naveed <m.asfandnaveed@gmail.com>
2023-08-04 14:48:37 +0500

    04-08-2023

--- lib/Nutrition_design/Nutrition_home.dart
+++ lib/Nutrition_design/Nutrition_home.dart
@@ -16,6 +16,7 @@ class _NutritionHomePageState extends State<NutritionHomePage> {
         child: Container(
           padding: EdgeInsets.all(30),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -23,7 +24,40 @@ class _NutritionHomePageState extends State<NutritionHomePage> {
                   SvgPicture.asset('assets/svg/menu.svg'),
                   SvgPicture.asset('assets/svg/shopping-cart.svg'),
                 ],
+
               ),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                'Nutri Delicious \nfood for you',
+                style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+
+              SizedBox(
+                height: 30,
+              ),
+              
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                decoration: BoxDecoration(
+                  color: Color(0xffEFEEEE),
+                  borderRadius: BorderRadius.circular(60),
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    icon: Icon(
+                      Icons.search,
+                    ),
+                    border: InputBorder.none
+                  ),
+                ),
+              ),
+
+
             ],
           ),
         ),
